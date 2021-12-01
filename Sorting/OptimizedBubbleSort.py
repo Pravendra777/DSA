091c6e4986d7dd8b8f9cf412eea92a60dc9ff578
Pravendra777 <prave757634@gmail.com>
2021-12-01 10:26:33 +0530

    sort

--- Sorting/OptimizedBubbleSort.py
+++ Sorting/OptimizedBubbleSort.py
@@ -1,10 +1,15 @@
 def bubble_sort(lst):
-    count=0
     for i in range(len(lst)):
+        count=0
+        swap=0
         for j in range(0,(len(lst)-i)-1):
             if lst[j]>lst[j+1]:
                 lst[j],lst[j+1]=lst[j+1],lst[j]
+                swap=1
                 count+=1
+        if swap==0:
+            print("no swapping for ",i+1,"iteration , list has been sorted in ",i,"iteration")
+            break
         print("sawp for", i+1,"iteration","=",count)
         count=0
     return lst
